b63de256b4095bcc3ee2dccddfe32aa71956356c
AZBZ34 <alexbeazley18@outlook.com>
2021-10-17 08:27:00 +1100

    init mobile deploy

---
+++ android/app/src/main/java/com/beazy/recipes/ionic/ng/MainActivity.java
@@ -0,0 +1,5 @@
+package com.beazy.recipes.ionic.ng;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.beazy.recipes.ionic.ng',
+  appName: 'ionic-ng',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
